b5d9820f18a0c8c732d4899699f91bb5d039c9d6
KTnomis <70838982+KTnomis@users.noreply.github.com>
2020-11-15 15:58:35 +0700

    Upddates
    new java, css, html changes

--- Test.js
+++ Test.js
@@ -15,3 +15,51 @@ function openTAB(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+var header = document.getElementById("tabcontent");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+//tracker effect
+let mouse = {x: 0, y: 0}
+let dist = 0
+let kid = document.getElementById("kid")
+
+const obj = {
+  x: kid.offsetLeft + kid.offsetWidth / 2, 
+  y: kid.offsetTop + kid.offsetHeight / 2
+}
+
+kid.style.transform = "translate(0px, -"+ kid.offsetHeight+"px)"
+
+window.addEventListener("mousemove", el => {
+  console.dir(el)
+  mouse = {x: el.x, y: el.y}
+  
+  dist = vec2.dist(
+    vec2.fromValues(obj.x, obj.y),
+    vec2.fromValues(mouse.x, mouse.y))  
+  
+  
+  if(dist < 150){
+    let translate = "translate("
+    + (mouse.x - obj.x) / 3+
+    "px,"
+    + (mouse.y - obj.y) / 3 +
+    "px)"
+    
+    kid.style.transition = "0.2s"
+    kid.style.transform = translate
+    
+    body.style.cursor = "crosshair"
+  }else{
+    kid.style.transition = "0.2s ease"
+    kid.style.transform = "translate(0px, -"+ kid.offsetHeight / 2+"px)"
+  }
+})
